d148db27540c23edf6577252923f995e31a32b5c
rpaternina <robertpaternina65@gmail.com>
2024-10-17 17:02:12 -0500

    commit desde local

---
+++ GestorTareas/src/main/java/com/mycompany/gestortareas/GestorTareas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestortareas;
+
+/**
+ *
+ * @author 57300
+ */
+public class GestorTareas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
