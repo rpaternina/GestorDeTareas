39a6f388592da9b56c3781b068cd2bc9fab7714e
rpaternina <robertpaternina65@gmail.com>
2024-10-19 12:47:20 -0500

    constructor y metodos a la clases

--- GestorTareas/src/main/java/com/mycompany/gestortareas/Tarea.java
+++ GestorTareas/src/main/java/com/mycompany/gestortareas/Tarea.java
@@ -1,11 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 package com.mycompany.gestortareas;
 
-
 public class Tarea {
+
+    private String titulo;
+    private String descripcion;
+    private boolean estado;
+
+    public Tarea(String titulo, String descripcion, boolean estado) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
 
 }
