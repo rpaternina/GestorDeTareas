6d7dfd32e741b1c1c7f67e3c01633fa432771cb1
rpaternina <robertpaternina65@gmail.com>
2024-10-19 12:38:43 -0500

    creacion de clases

--- GestorTareas/src/main/java/com/mycompany/gestortareas/GestorTareas.java
+++ GestorTareas/src/main/java/com/mycompany/gestortareas/GestorTareas.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.gestortareas;
 
-/**
- *
- * @author 57300
- */
 public class GestorTareas {
 
     public static void main(String[] args) {
